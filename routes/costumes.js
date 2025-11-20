79e052bdad802a1d4a44061ddebc58e4d1335a95
Chanakya Chowdary <218w1a05c1@vrsec.ac.in>
2025-11-19 21:28:37 -0600

    Add detail view route, controller, and template

--- routes/costumes.js
+++ routes/costumes.js
@@ -5,4 +5,7 @@ var router = express.Router();
 /* GET costumes */
 router.get('/', costume_controller.costume_view_all_Page);
 
+/* GET detail costume page */
+router.get('/detail', costume_controller.costume_view_one_Page);
+
 module.exports = router;

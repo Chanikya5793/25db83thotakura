5ebe41d5132a090284359c16ca72edacfd98a383
Chanakya Chowdary <218w1a05c1@vrsec.ac.in>
2025-12-03 21:09:00 -0600

    Refactor cost attribute in costume model to include validation rules

--- models/costume.js
+++ models/costume.js
@@ -4,7 +4,11 @@ const costumeSchema = mongoose.Schema({
   costume_type: String,
   size: String,
   color: String,
-  cost: Number
+  cost: {
+    type: Number,
+    min: [0, 'Cost cannot be negative'],
+    max: [1000, 'Cost cannot exceed 1000']
+  }
 });
 
 module.exports = mongoose.model("Costume", costumeSchema);

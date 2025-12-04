5a3bde7fbe080497643555bdc17b5cf1173a90e5
Chanakya Chowdary <218w1a05c1@vrsec.ac.in>
2025-12-03 21:26:55 -0600

    Bonus: Add size/type validation and protect create/delete endpoints

--- models/costume.js
+++ models/costume.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose");
 
 const costumeSchema = mongoose.Schema({
-  costume_type: String,
-  size: String,
+  costume_type: {
+    type: String,
+    required: [true, 'Costume type is required'],
+    minlength: [2, 'Costume type must be at least 2 characters']
+  },
+  size: {
+    type: String,
+    enum: {
+      values: ['small', 'medium', 'large', 'xlarge'],
+      message: 'Size must be small, medium, large, or xlarge'
+    }
+  },
   color: String,
   cost: {
     type: Number,
--- routes/costumes.js
+++ routes/costumes.js
@@ -18,12 +18,12 @@ router.get('/', costume_controller.costume_view_all_Page);
 router.get('/detail', costume_controller.costume_view_one_Page);
 
 /* GET create costume page */
-router.get('/create', costume_controller.costume_create_Page);
+router.get('/create', secured, costume_controller.costume_create_Page);
 
 /* GET update costume page */
 router.get('/update', secured, costume_controller.costume_update_Page);
 
 /* GET delete costume page */
-router.get('/delete', costume_controller.costume_delete_Page);
+router.get('/delete', secured, costume_controller.costume_delete_Page);
 
 module.exports = router;

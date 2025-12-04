3083db5207f54996b74f4fb5592ca8528fbe6905
Chanakya Chowdary <218w1a05c1@vrsec.ac.in>
2025-12-03 20:48:28 -0600

    Protect update endpoint with secured middleware

--- routes/costumes.js
+++ routes/costumes.js
@@ -2,6 +2,15 @@ var express = require('express');
 const costume_controller = require('../controllers/costume');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on
+// or redirect to login.
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  res.redirect("/login");
+};
+
 /* GET costumes */
 router.get('/', costume_controller.costume_view_all_Page);
 
@@ -12,7 +21,7 @@ router.get('/detail', costume_controller.costume_view_one_Page);
 router.get('/create', costume_controller.costume_create_Page);
 
 /* GET update costume page */
-router.get('/update', costume_controller.costume_update_Page);
+router.get('/update', secured, costume_controller.costume_update_Page);
 
 /* GET delete costume page */
 router.get('/delete', costume_controller.costume_delete_Page);
